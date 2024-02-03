edecf7d8f387a39d5e6027682460349a5931c94a
darshan45672 <drshnbhandary@gmail.com>
2024-02-03 20:52:47 +0530

    added home page and layout page

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,12 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Udyog</title>
-</head>
-<body>
-    <h1>home</h1>
-</body>
-</html>
+@extends('front.layout.app')
