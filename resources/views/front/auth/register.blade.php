601d32b4af40674e4703f87a2265aa08be75a2b8
darshan45672 <drshnbhandary@gmail.com>
2024-02-04 15:28:26 +0530

    updated routes in home page

--- resources/views/front/auth/register.blade.php
+++ resources/views/front/auth/register.blade.php
@@ -30,7 +30,7 @@
                     </form>                    
                 </div>
                 <div class="mt-4 text-center">
-                    <p>Have an account? <a  href="login.html">Login</a></p>
+                    <p>Have an account? <a  href="{{ route('login') }}">Login</a></p>
                 </div>
             </div>
         </div>
