9537285baeae7132f974056ffd2765015e5a0c34
darshan45672 <drshnbhandary@gmail.com>
2024-02-05 07:55:36 +0530

    added form validation

--- resources/views/front/auth/register.blade.php
+++ resources/views/front/auth/register.blade.php
@@ -12,19 +12,19 @@
                         @csrf
                         <div class="form-group mb-3">
                             <label for="name" class="mb-2">Name*</label>
-                            <input type="text" name="name" id="name" class="form-control" placeholder="Enter Name">
+                            <input type="text" name="name" id="name" class="form-control" placeholder="Enter Name" required >
                         </div> 
                         <div class="form-group mb-3">
                             <label for="email" class="mb-2">Email*</label>
-                            <input type="email" name="email" id="email" class="form-control" placeholder="Enter Email">
+                            <input type="email" name="email" id="email" class="form-control" placeholder="Enter Email" required>
                         </div> 
                         <div class="form-group mb-3">
                             <label for="password" class="mb-2">Password*</label>
-                            <input type="password" name="password" id="password" class="form-control" placeholder="Enter Password">
+                            <input type="password" name="password" id="password" class="form-control" placeholder="Enter Password" required>
                         </div> 
                         <div class="form-group mb-3">
                             <label for="confirm_password" class="mb-2">Confirm Password*</label>
-                            <input type="password" name="password_confirmation" id="confrim_password" class="form-control" placeholder="Enter Password">
+                            <input type="password" name="password_confirmation" id="confrim_password" class="form-control" placeholder="Enter Password" required>
                         </div> 
                         <button class="btn btn-primary mt-2">Register</button>
                     </form>                    
